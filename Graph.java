5771490ad0222f7afc7ec539ae45a7c9470f60c6
Sam Kiehl <e22-kiehlsg@elderhs.net>
2021-10-13 19:14:50 -0400

    Added auto and end-to-end graphing modes, added a (slightly) more elegant exit process.

--- Graph.java
+++ Graph.java
@@ -1,12 +1,27 @@
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 class Graph extends Canvas{  
+  private static Scanner input = new Scanner(System.in);
   private String name;
   private int xDim, yDim, multiplier;
   private ArrayList<Vector> vectors;
   private ArrayList<double[]> vBreakdowns;
-  public Graph(String name, int xDim, int yDim, int multiplier, ArrayList<Vector> vectors) {  
-    this.name = name + ": ["; 
+  private int mode; // 0: Auto(all resultants are graphed from the origin, all normal vectors are graphed end-to-end), 1: All end to end, 2: All from origin.
+  public Graph(String name, int xDim, int yDim, int multiplier, ArrayList<Vector> vectors, int mode) {  
+    this.xDim = xDim;
+    this.yDim = yDim;
+    this.multiplier = multiplier;
+    this.vectors = vectors;
+    this.vBreakdowns = new ArrayList<double[]>();
+    this.mode = mode;
+    this.name = name;
+    if(this.mode == 0)
+      this.name += " (auto) ";
+    else if(this.mode == 1)
+      this.name += " (end to end) ";
+    else if (this.mode  == 2)
+      this.name += " (all from origin) ";
     for(int i = 0; i < vectors.size(); i++){
       this.name += vectors.get(i).getName();
       if(i < vectors.size() - 1)
@@ -14,15 +29,15 @@ class Graph extends Canvas{
     }
     this.name += "]";
     
-    this.xDim = xDim;
-    this.yDim = yDim;
-    this.multiplier = multiplier;
-    this.vectors = vectors;
-    this.vBreakdowns = new ArrayList<double[]>();
     setBackground(Color.BLACK);
     setSize(xDim, yDim);
-    for(Vector v : this.vectors)
-      this.vBreakdowns.add(this.breakdown(v));
+    if(this.mode == 0)
+      this.vBreakdowns = breakdownAuto(this.vectors);
+    else if(this.mode == 1)
+      this.vBreakdowns = breakdownEToE(this.vectors);
+    else if(this.mode == 2)
+      this.vBreakdowns = breakdown(this.vectors);
+    
     this.setup();
   }    
 
@@ -50,15 +65,73 @@ class Graph extends Canvas{
 
   }
 
-  public double[] breakdown(Vector v){ // Returns an integer array used to graph a vector in the Graph class.
+  public double[] breakdown(Vector v){ // Returns an integer array used to graph a vector in the Graph class. (converts iHat, jHat into (x1, x2, y1, y2))
     double x1, y1, x2, y2;
     x1 = this.xDim / 2;
-    y1 = this.yDim / 2;             //FIX
+    y1 = this.yDim / 2;
     x2 = x1 + this.multiplier * v.getIHat();
     y2 = y1 - this.multiplier * v.getJHat();
 
     return new double[]{x1, y1, x2, y2};
   }
+
+  public ArrayList<double[]> breakdown(ArrayList<Vector> vectors){ // Returns an Vector ArrayList used to graph a vector in the Graph class.(converts iHat, jHat into (x1, x2, y1, y2))
+    ArrayList<double[]> output = new ArrayList<double[]>();
+    for(Vector v : vectors){
+    double x1, y1, x2, y2;
+      x1 = this.xDim / 2;
+      y1 = this.yDim / 2;
+      x2 = x1 + this.multiplier * v.getIHat();
+      y2 = y1 - this.multiplier * v.getJHat();
+
+      output.add(new double[]{x1, y1, x2, y2});
+    }
+    return output;
+  }
+
+  public ArrayList<double[]> breakdownEToE(ArrayList<Vector> vectors){
+    ArrayList<double[]> output = new ArrayList<double[]>();
+    for(int i = 0; i < vectors.size(); i++){
+      double x1, y1, x2, y2;
+      if(i == 0){
+        x1 = this.xDim / 2;
+        y1 = this.yDim / 2;
+      }else{
+        x1 = output.get(i - 1)[2];
+        y1 = output.get(i - 1)[3];
+      }
+        x2 = x1 + this.multiplier * vectors.get(i).getIHat();
+        y2 = y1 - this.multiplier * vectors.get(i).getJHat();
+        output.add(new double[]{x1, y1, x2, y2});
+      }
+      return output;
+  }
+
+  public ArrayList<double[]> breakdownAuto(ArrayList<Vector> vectors){
+    ArrayList<double[]> output = new ArrayList<double[]>();
+    ArrayList<double[]> resultants = new ArrayList<double[]>();
+    for(int i = 0; i < vectors.size(); i++){
+      if (vectors.get(i).getName().contains("Resultant"))
+        resultants.add(breakdown(vectors.get(i)));
+      else{
+        double x1, y1, x2, y2;
+        if(i == 0){
+          x1 = this.xDim / 2;
+          y1 = this.yDim / 2;
+        }else{
+          x1 = output.get(i - 1)[2];
+          y1 = output.get(i - 1)[3];
+        }
+          x2 = x1 + this.multiplier * vectors.get(i).getIHat();
+          y2 = y1 - this.multiplier * vectors.get(i).getJHat();
+          output.add(new double[]{x1, y1, x2, y2});
+        }
+      }
+      for(double[] d : resultants)
+        output.add(d);
+      return output;
+  }
+  
 
   public String toString(){
     return this.name + " has been graphed.";
@@ -68,8 +141,22 @@ class Graph extends Canvas{
     ArrayList<Vector> v = new ArrayList<Vector>();
     v.add(new Vector("v1", -14.142, 14.142));
     v.add(new Vector("v2", 0, -20));
-    v.add(v.get(0).minus(v.get(1)));
+    v.add(v.get(0).plus(v.get(1)));
 
-    Graph graph = new Graph("Graph", 700, 700, 5, v);
+    Graph graph = new Graph("Graph", 700, 700, 5, v, 0);
+    Graph graph1 = new Graph("Graph", 700, 700, 5, v, 1);
+    Graph graph2 = new Graph("Graph", 700, 700, 5, v, 2);
+
+
+    
+    
+    
+    // Exit stuff
+    String in = "...";
+    while(!in.equals("")){
+      System.out.print("Press [Enter] to close graph(s).");
+      in = input.nextLine();
+    }
+    System.exit(0);
   }
 }       
